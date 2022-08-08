be2602b05737d4adc63c70701f298ed6e2f095c3
Denk <komedian@bk.ru>
2022-08-08 20:38:42 +0300

    dataset

--- segmentation_baseline/datasets.py
+++ segmentation_baseline/datasets.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandas as pd
 from torch.utils.data import Dataset
 
-from .utils import preprocess_image, preprocess_mask2onehot, preprocess_single_mask, get_img_names
+from .utils import preprocess_image, preprocess_mask2onehot, preprocess_single_mask, get_img_names, rle2mask
 
 
 class MulticlassDataset(Dataset):
@@ -82,7 +82,7 @@ class BinaryDataset(Dataset):
 
 
 class HubmapDataset(Dataset):
-    def __init__(self, images_dir, masks_dir, csv_path, labels=None, img_w=None, img_h=None, augs=None):
+    def __init__(self, images_dir, masks_dir, csv_path, stage, labels=None, img_w=None, img_h=None, augs=None):
         self.df = pd.read_csv(csv_path)
         self.images_dir = images_dir
         self.masks_dir = masks_dir
@@ -90,6 +90,7 @@ class HubmapDataset(Dataset):
         self.img_w = img_w
         self.img_h = img_h
         self.augs = augs
+        self.stage = stage
 
         self.normalizers = self.get_stain_normalizers()
         
@@ -111,8 +112,8 @@ class HubmapDataset(Dataset):
             normalizers.append(normalizer)
         return normalizers
 
-    def get_mixup(self, df, organ):
-        sub_df = df[df['organ'] == organ].sample(n=4, replace=True)
+    def get_mixup(self, df):
+        sub_df = df.sample(n=4, replace=True)
 
         img_ids = [row['id'] for i, row in sub_df.iterrows()]
         np.random.shuffle(img_ids)
@@ -161,9 +162,8 @@ class HubmapDataset(Dataset):
     def __getitem__(self, index):
         info = self.df.iloc[index]
         
-        if np.random.random() < 0.1:
-            organ = info['organ']
-            image, mask = self.get_mixup(self.df, organ)
+        if self.stage == 'train' and np.random.random() < 0.15:
+            image, mask = self.get_mixup(self.df)
         else:
             img_name = '{}.tiff'.format(info['id'])
             img_path = os.path.join(self.images_dir, img_name)
@@ -171,15 +171,131 @@ class HubmapDataset(Dataset):
             image = cv2.imread(img_path)
             img_h, img_w = image.shape[:2]
 
-            if np.random.random() > 0.5:
+            if self.stage == 'train' and np.random.random() < 0.5:
                 image = cv2.resize(image, dsize=None, fx=0.064, fy=0.064, interpolation=cv2.INTER_LINEAR)
                 image = cv2.resize(image, dsize=(img_w, img_h), interpolation=cv2.INTER_LINEAR)
             
-            if np.random.random() > 0.8:
+            if self.stage == 'train' and np.random.random() < 0.2:
                 normalizer = np.random.choice(self.normalizers) 
                 image, _, _ = normalizer.normalize(I=image, stains=True)
 
             mask = cv2.imread(msk_path, 0)
+
+            if self.augs is not None:
+                item = self.augs(image=image, mask=mask)
+                image = item['image']
+                mask = item['mask']
+
+        mean = np.array([0.485, 0.456, 0.406]) 
+        std = np.array([0.229, 0.224, 0.225])
+
+        image = preprocess_image(image, img_w=self.img_w, img_h=self.img_h, mean=mean, std=std)
+        mask = preprocess_single_mask(mask, self.labels, img_w=self.img_w, img_h=self.img_h)
+
+        return {
+            'image': image, 
+            'mask': mask.unsqueeze(0).float(),
+        }
+
+
+class HubmapDatasetFromRLE(Dataset):
+    def __init__(self, images_dir, csv_path, stage, labels=None, img_w=None, img_h=None, augs=None):
+        self.df = pd.read_csv(csv_path)
+        self.images_dir = images_dir
+        self.labels = labels if labels else [0, 1]
+        self.img_w = img_w
+        self.img_h = img_h
+        self.augs = augs
+        self.stage = stage
+
+        self.normalizers = self.get_stain_normalizers()
+        
+    def __len__(self):
+        return self.df.shape[0]
+
+    def get_stain_normalizers(self):
+        import torchstain as ts
+        targets = [
+            cv2.imread('./stain_images/1.jpg'),
+            cv2.imread('./stain_images/2.jpg'),
+            cv2.imread('./stain_images/3.jpg'),
+            cv2.imread('./stain_images/4.jpg'),
+        ] 
+        normalizers = []
+        for target in targets:
+            normalizer = ts.MacenkoNormalizer(backend='numpy')
+            normalizer.fit(target)
+            normalizers.append(normalizer)
+        return normalizers
+
+    def get_mixup(self, df):
+        sub_df = df.sample(n=4, replace=True)
+
+        img_ids = [row['id'] for i, row in sub_df.iterrows()]
+        mask_rles = [row['rle'] for i, row in sub_df.iterrows()]
+        np.random.shuffle(img_ids)
+
+        half_h = self.img_h // 2
+        half_w = self.img_w // 2
+
+        r_image = np.zeros((self.img_h, self.img_w, 3), dtype=np.uint8)
+        r_mask = np.zeros((self.img_h, self.img_w), dtype=np.uint8)
+
+        for i, img_id in enumerate(img_ids):
+            img_name = f'{img_id}.jpg'
+            img_path = os.path.join(self.images_dir, img_name)
+
+            image = cv2.imread(img_path)
+            img_h, img_w = image.shape[:2]
+            mask = rle2mask(mask_rles[i], (img_h, img_w))
+
+            if np.random.random() > 0.5:
+                img_h, img_w = image.shape[:2]
+                image = cv2.resize(image, dsize=None, fx=0.064, fy=0.064, interpolation=cv2.INTER_LINEAR)
+                image = cv2.resize(image, (img_w, img_h), interpolation=cv2.INTER_LINEAR)
+
+            image = cv2.resize(image, (self.img_w, self.img_h), interpolation=cv2.INTER_LINEAR)
+            mask = cv2.resize(mask, (self.img_w, self.img_h), interpolation=cv2.INTER_LINEAR)
+
+            if np.random.random() > 0.5:
+                normalizer = np.random.choice(self.normalizers) 
+                image, _, _ = normalizer.normalize(I=image, stains=True)
+
+            if i == 0:
+                r_image[0:half_h, 0:half_w] = image[0:half_h, 0:half_w].copy()
+                r_mask[0:half_h, 0:half_w] = mask[0:half_h, 0:half_w].copy()
+            elif i == 1:
+                r_image[0:half_h, half_w:] = image[0:half_h, half_w:].copy()
+                r_mask[0:half_h, half_w:] = mask[0:half_h, half_w:].copy()
+            elif i == 2:
+                r_image[half_h:, 0:half_w] = image[half_h:, 0:half_w].copy()
+                r_mask[half_h:, 0:half_w] = mask[half_h:, 0:half_w].copy()
+            elif i == 3:
+                r_image[half_h:, half_w:] = image[half_h:, half_w:].copy()
+                r_mask[half_h:, half_w:] = mask[half_h:, half_w:].copy()
+        
+        return r_image, r_mask
+
+    def __getitem__(self, index):
+        info = self.df.iloc[index]
+        
+        if self.stage == 'train' and np.random.random() < 0.15:
+            image, mask = self.get_mixup(self.df)
+        else:
+            img_name = '{}.jpg'.format(info['id'])
+            img_path = os.path.join(self.images_dir, img_name)
+            image = cv2.imread(img_path)
+            img_h, img_w = image.shape[:2]
+
+            if self.stage == 'train' and np.random.random() > 0.5:
+                image = cv2.resize(image, dsize=None, fx=0.064, fy=0.064, interpolation=cv2.INTER_LINEAR)
+                image = cv2.resize(image, dsize=(img_w, img_h), interpolation=cv2.INTER_LINEAR)
+            
+            if self.stage == 'train' and np.random.random() < 0.2:
+                normalizer = np.random.choice(self.normalizers) 
+                image, _, _ = normalizer.normalize(I=image, stains=True)
+
+            mask = rle2mask(info['rle'], (img_h, img_w))
 
             if self.augs is not None:
                 item = self.augs(image=image, mask=mask)
