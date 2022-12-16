6480a3782c5868ed41ee985fcb4354abc0249fb3
Denis <komedian@bk.ru>
2022-12-16 10:40:52 +0300

    drop_rate and drop_path_rate

--- denk_baseline/custom_models/classification/timm_default.py
+++ denk_baseline/custom_models/classification/timm_default.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 
 
 class TimmNet(nn.Module):
-    def __init__(self, model_name, num_classes, in_chans=3, pretrained=False, global_pool='max'):
+    def __init__(self, model_name, num_classes, in_chans=3, pretrained=False, global_pool='max', drop_rate=0.3, drop_path_rate=0.2):
         super().__init__()
         self.backbone = timm.create_model(
             model_name=model_name, 
@@ -13,6 +13,8 @@ class TimmNet(nn.Module):
             in_chans=in_chans, 
             pretrained=pretrained,
             global_pool=global_pool,
+            drop_rate=drop_rate,
+            drop_path_rate=drop_path_rate,
         )
 
     def forward(self, x):
